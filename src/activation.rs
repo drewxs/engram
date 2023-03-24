2c57ae87b4ab4bf09c35e5f67963183f2ff1ffcb
Andrew X. Shah <drewshah0@gmail.com>
2023-03-23 21:54:25 -0600

    feat(activation): impl apply and gradient

--- src/activation.rs
+++ src/activation.rs
@@ -11,13 +11,10 @@ pub fn relu(x: f64) -> f64 {
 }
 
 pub fn leaky_relu(x: f64) -> f64 {
-    if x > 0.0 {
-        x
-    } else {
-        0.01 * x
-    }
+    f64::max(x, 0.01 * x)
 }
 
+#[derive(Debug)]
 pub enum Activation {
     Sigmoid,
     TanH,
@@ -28,5 +25,37 @@ pub enum Activation {
 impl Default for Activation {
     fn default() -> Self {
         Self::Sigmoid
+    }
+}
+
+impl Activation {
+    pub fn apply(&self, x: f64) -> f64 {
+        match self {
+            Self::Sigmoid => sigmoid(x),
+            Self::TanH => tanh(x),
+            Self::ReLU => relu(x),
+            Self::LeakyReLU => leaky_relu(x),
+        }
+    }
+
+    pub fn gradient(&self, x: f64) -> f64 {
+        match self {
+            Self::Sigmoid => sigmoid(x) * (1.0 - sigmoid(x)),
+            Self::TanH => 1.0 - tanh(x).powi(2),
+            Self::ReLU => {
+                if x > 0.0 {
+                    1.0
+                } else {
+                    0.0
+                }
+            }
+            Self::LeakyReLU => {
+                if x > 0.0 {
+                    1.0
+                } else {
+                    0.01
+                }
+            }
+        }
     }
 }
