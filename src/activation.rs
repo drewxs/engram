5b7c7dd8fabade4f0a2658e13ccbb69dcdbec3e6
Andrew X. Shah <drewshah0@gmail.com>
2023-04-08 12:57:23 -0600

    feat(activation): add methods for activating tensors

--- src/activation.rs
+++ src/activation.rs
@@ -1,3 +1,5 @@
+use crate::Tensor;
+
 /// Activation functions and their derivatives
 #[derive(Clone, Copy, Debug)]
 pub enum Activation {
@@ -18,26 +20,24 @@ impl Activation {
         }
     }
 
+    /// Returns a tensor with the activation function applied to each element.
+    pub fn apply_tensor(&self, tensor: &Tensor) -> Tensor {
+        tensor.mapv(&|x| self.apply(x))
+    }
+
     /// Returns the derivative of the activation function.
     pub fn gradient(&self, x: f64) -> f64 {
         match self {
-            Self::Sigmoid => Self::sigmoid(x) * (1.0 - Self::sigmoid(x)),
-            Self::TanH => 1.0 - Self::tanh(x).powi(2),
-            Self::ReLU => {
-                if x > 0.0 {
-                    1.0
-                } else {
-                    0.0
-                }
-            }
-            Self::LeakyReLU => {
-                if x > 0.0 {
-                    1.0
-                } else {
-                    0.01
-                }
-            }
+            Self::Sigmoid => Self::sigmoid_derivative(x),
+            Self::TanH => 1.0 - Self::tanh_derivative(x),
+            Self::ReLU => Self::relu_derivative(x),
+            Self::LeakyReLU => Self::leaky_relu_derivative(x),
         }
+    }
+
+    /// Returns a tensor with the derivative of the activation function applied to each element.
+    pub fn gradient_tensor(&self, tensor: &Tensor) -> Tensor {
+        tensor.mapv(&|x| self.gradient(x))
     }
 
     /// Returns the result of the sigmoid function.
@@ -45,9 +45,19 @@ impl Activation {
         1.0 / (1.0 + (-x).exp())
     }
 
+    /// Returns the derivative of the sigmoid function.
+    fn sigmoid_derivative(x: f64) -> f64 {
+        Self::sigmoid(x) * (1.0 - Self::sigmoid(x))
+    }
+
     /// Returns the result of the hyperbolic tangent function.
     fn tanh(x: f64) -> f64 {
         x.tanh()
+    }
+
+    /// Returns the derivative of the hyperbolic tangent function.
+    fn tanh_derivative(x: f64) -> f64 {
+        1.0 - Self::tanh(x).powi(2)
     }
 
     /// Returns the result of the rectified linear unit function.
@@ -55,8 +65,26 @@ impl Activation {
         f64::max(x, 0.0)
     }
 
+    /// Returns the derivative of the rectified linear unit function.
+    fn relu_derivative(x: f64) -> f64 {
+        if x > 0.0 {
+            1.0
+        } else {
+            0.0
+        }
+    }
+
     /// Returns the result of the leaky rectified linear unit function.
     fn leaky_relu(x: f64) -> f64 {
         f64::max(x, 0.01 * x)
+    }
+
+    /// Returns the derivative of the leaky rectified linear unit function.
+    fn leaky_relu_derivative(x: f64) -> f64 {
+        if x > 0.0 {
+            1.0
+        } else {
+            0.01
+        }
     }
 }
