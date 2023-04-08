d423532d0d3abd5835ebc36263e896a050ca7b32
Andrew X. Shah <drewshah0@gmail.com>
2023-04-08 02:17:38 -0600

    fix(activation): make individual activation functions private

--- src/activation.rs
+++ src/activation.rs
@@ -11,18 +11,18 @@ impl Activation {
     /// Returns the result of the activation function.
     pub fn apply(&self, x: f64) -> f64 {
         match self {
-            Self::Sigmoid => sigmoid(x),
-            Self::TanH => tanh(x),
-            Self::ReLU => relu(x),
-            Self::LeakyReLU => leaky_relu(x),
+            Self::Sigmoid => Self::sigmoid(x),
+            Self::TanH => Self::tanh(x),
+            Self::ReLU => Self::relu(x),
+            Self::LeakyReLU => Self::leaky_relu(x),
         }
     }
 
     /// Returns the derivative of the activation function.
     pub fn gradient(&self, x: f64) -> f64 {
         match self {
-            Self::Sigmoid => sigmoid(x) * (1.0 - sigmoid(x)),
-            Self::TanH => 1.0 - tanh(x).powi(2),
+            Self::Sigmoid => Self::sigmoid(x) * (1.0 - Self::sigmoid(x)),
+            Self::TanH => 1.0 - Self::tanh(x).powi(2),
             Self::ReLU => {
                 if x > 0.0 {
                     1.0
@@ -39,20 +39,24 @@ impl Activation {
             }
         }
     }
-}
 
-pub fn sigmoid(x: f64) -> f64 {
-    1.0 / (1.0 + (-x).exp())
-}
+    /// Returns the result of the sigmoid function.
+    fn sigmoid(x: f64) -> f64 {
+        1.0 / (1.0 + (-x).exp())
+    }
 
-pub fn tanh(x: f64) -> f64 {
-    x.tanh()
-}
+    /// Returns the result of the hyperbolic tangent function.
+    fn tanh(x: f64) -> f64 {
+        x.tanh()
+    }
 
-pub fn relu(x: f64) -> f64 {
-    f64::max(x, 0.0)
-}
+    /// Returns the result of the rectified linear unit function.
+    fn relu(x: f64) -> f64 {
+        f64::max(x, 0.0)
+    }
 
-pub fn leaky_relu(x: f64) -> f64 {
-    f64::max(x, 0.01 * x)
+    /// Returns the result of the leaky rectified linear unit function.
+    fn leaky_relu(x: f64) -> f64 {
+        f64::max(x, 0.01 * x)
+    }
 }
--- tests/activation_spec.rs
+++ tests/activation_spec.rs
@@ -1,25 +1,29 @@
-use engram::activation;
+use engram::activation::Activation;
 
 #[test]
 fn it_calculates_sigmoid() {
-    let result = activation::sigmoid(0.0);
+    let activation = Activation::Sigmoid;
+    let result = activation.apply(0.0);
     assert_eq!(result, 0.5);
 }
 
 #[test]
 fn it_calculates_tanh() {
-    let result = activation::tanh(0.0);
+    let activation = Activation::TanH;
+    let result = activation.apply(0.0);
     assert_eq!(result, 0.0);
 }
 
 #[test]
 fn it_calculates_relu_0() {
-    let result = activation::relu(1.0);
+    let activation = Activation::ReLU;
+    let result = activation.apply(1.0);
     assert_eq!(result, 1.0);
 }
 
 #[test]
 fn it_calculates_leaky_relu() {
-    let result = activation::leaky_relu(-1.0);
+    let activation = Activation::LeakyReLU;
+    let result = activation.apply(-1.0);
     assert_eq!(result, -0.01);
 }
