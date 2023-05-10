acef9452d2af2dc7fc18cdff745e8d81cd0d5c69
Andrew X. Shah <drewshah0@gmail.com>
2023-05-10 03:31:49 -0600

    docs(layer): add examples

--- src/layer.rs
+++ src/layer.rs
@@ -18,12 +18,24 @@ pub struct Layer {
 impl Layer {
     /// Creates a new `Layer` with the specified number of input and output neurons,
     /// using the specified initialization technique.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use engram::{Layer, Initializer};
+    /// let layer = Layer::new(2, 3, &Initializer::Xavier);
+    /// assert_eq!(layer.weights.shape(), (2, 3));
+    /// assert_eq!(layer.biases.shape(), (3, 1));
+    /// assert_eq!(layer.d_weights.shape(), (2, 3));
+    /// assert_eq!(layer.d_biases.shape(), (1, 3));
+    /// assert!(layer.output.is_none());
+    /// ```
     pub fn new(f_in: usize, f_out: usize, initializer: &Initializer) -> Layer {
         let weights = Tensor::initialize(f_in, f_out, initializer);
         let biases = Tensor::initialize(f_out, 1, initializer);
         let d_weights = Tensor::zeros(f_in, f_out);
         let d_biases = Tensor::zeros(1, f_out);
-        let output = None; // should have dimensions (f_out, 1)
+        let output = None;
 
         Layer {
             weights,
@@ -35,6 +47,16 @@ impl Layer {
     }
 
     /// Feeds the input through the layer, returning the output.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use engram::{Activation, Initializer, Layer, tensor};
+    /// let mut layer = Layer::new(2, 3, &Initializer::Xavier);
+    /// let inputs = tensor![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
+    /// let output = layer.feed_forward(&inputs, Activation::Sigmoid);
+    /// assert_eq!(output.shape(), (3, 3));
+    /// ```
     pub fn feed_forward(&mut self, inputs: &Tensor, activation: Activation) -> Tensor {
         let output = activation.apply_tensor(&(self.weights).matmul(inputs).add(&self.biases));
         self.output = Some(output.clone());
