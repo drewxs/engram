53fd0c32f357007c91cad58bfdb37fa3e5f7e3d8
Andrew X. Shah <drewshah0@gmail.com>
2023-09-04 19:17:22 -0600

    feat(loss): create loss trait and enum

--- src/loss/mod.rs
+++ src/loss/mod.rs
@@ -5,3 +5,23 @@ pub mod mse;
 
 pub use bce::*;
 pub use mse::*;
+
+use crate::Tensor;
+
+pub enum LossFunction {
+    BinaryCrossEntropy,
+    MeanSquaredError,
+}
+
+pub trait Loss {
+    fn loss(&self, predictions: &Tensor, targets: &Tensor) -> f64;
+}
+
+impl Loss for LossFunction {
+    fn loss(&self, predictions: &Tensor, targets: &Tensor) -> f64 {
+        match self {
+            LossFunction::BinaryCrossEntropy => binary_cross_entropy(predictions, targets),
+            LossFunction::MeanSquaredError => mean_squared_error(predictions, targets),
+        }
+    }
+}
