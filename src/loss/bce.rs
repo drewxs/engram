b4879e4fa609745c84b22a9f5dfdccb5ea624d4f
Andrew X. Shah <drewshah0@gmail.com>
2023-09-04 19:08:55 -0600

    docs(loss): add descriptions and tests

--- src/loss/bce.rs
+++ src/loss/bce.rs
@@ -1,13 +1,36 @@
+//! Binary cross entropy loss.
+
 use crate::Tensor;
 
-/// Binary cross entropy loss.
+/// Returns the binary cross entropy between the predictions and the targets.
+///
+/// # Examples
+///
+/// ```
+/// # use engram::{tensor, loss::binary_cross_entropy};
+/// let predictions = tensor![[0.9, 0.1, 0.2], [0.1, 0.9, 0.8]];
+/// let targets = tensor![[1.0, 0.0, 0.0], [0.0, 1.0, 1.0]];
+/// let loss = binary_cross_entropy(&predictions, &targets);
+/// assert_eq!(loss, 0.14462152754328741);
+/// ```
 pub fn binary_cross_entropy(predictions: &Tensor, targets: &Tensor) -> f64 {
-    let epsilon = 1e-8; // Small value to avoid taking the logarithm of zero
-    let loss = &(predictions.add_scalar(epsilon).ln().mul(&targets).add(
-        &targets
-            .sub_scalar(1.0)
-            .mul(&predictions.sub_scalar(1.0).add_scalar(epsilon).ln()),
-    ))
-    .mul_scalar(-1.0);
-    loss.mean()
+    assert_eq!(
+        predictions.shape(),
+        targets.shape(),
+        "Shapes of predictions and targets must match."
+    );
+
+    let epsilon = 1e-15; // Small constant to avoid log(0)
+    let predictions = predictions.clip(epsilon, 1.0 - epsilon);
+    let ones = Tensor::ones_like(&predictions);
+    let predictions_complement = ones.sub(&predictions);
+    let targets_complement = ones.sub(&targets);
+
+    let loss = targets
+        .mul(&predictions.ln())
+        .add(&targets_complement.mul(&predictions_complement.ln()))
+        .mul_scalar(-1.0);
+    let mean_loss = loss.mean();
+
+    mean_loss
 }
--- src/loss/mse.rs
+++ src/loss/mse.rs
@@ -1,6 +1,18 @@
+//! Mean squared error loss.
+
 use crate::Tensor;
 
-/// Mean squared error loss.
+/// Returns mean squared error between the predictions and the targets.
+///
+/// # Examples
+///
+/// ```
+/// # use engram::{tensor, loss::mean_squared_error};
+/// let predictions = tensor![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]];
+/// let targets = tensor![[1.2, 2.4, 3.1], [4.4, 4.7, 5.9]];
+/// let loss = mean_squared_error(&predictions, &targets);
+/// assert_eq!(loss, 0.07833333333333335);
+/// ```
 pub fn mean_squared_error(predictions: &Tensor, targets: &Tensor) -> f64 {
-    predictions.sub(targets).pow(2.0).mean()
+    predictions.sub(targets).square().mean()
 }
