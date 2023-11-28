64fa5b63cac4373f4ac1375ec8cfeb2bd4e42c39
Andrew X. Shah <drewshah0@gmail.com>
2023-11-28 00:08:05 -0700

    fix(tensor/ops): allow ops with diff dims

--- src/linalg/tensor/ops.rs
+++ src/linalg/tensor/ops.rs
@@ -13,12 +13,16 @@ impl Tensor {
     /// assert_eq!(c.data, vec![vec![6.0, 8.0], vec![10.0, 12.0]]);
     /// ```
     pub fn add(&self, other: &Tensor) -> Tensor {
-        self.validate_shape(other, "add");
-
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] + other.data[i][j];
+                if j >= other.cols {
+                    break;
+                }
+                res.data[i][j] += other.data[i][j];
             }
         }
         res
@@ -36,10 +40,14 @@ impl Tensor {
     /// assert_eq!(a.data, vec![vec![6.0, 8.0], vec![10.0, 12.0]]);
     /// ```
     pub fn add_mut(&mut self, other: &Tensor) {
-        self.validate_shape(other, "add_mut");
-
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
+                if j >= other.cols {
+                    break;
+                }
                 self.data[i][j] += other.data[i][j];
             }
         }
@@ -56,10 +64,10 @@ impl Tensor {
     /// assert_eq!(b.data, vec![vec![6.0, 7.0], vec![8.0, 9.0]]);
     /// ```
     pub fn add_scalar(&self, scalar: f64) -> Tensor {
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] + scalar;
+                res.data[i][j] += scalar;
             }
         }
         res
@@ -94,12 +102,16 @@ impl Tensor {
     /// let c = a.sub(&b);
     /// assert_eq!(c.data, vec![vec![-4.0, -4.0], vec![-4.0, -4.0]]);
     pub fn sub(&self, other: &Tensor) -> Tensor {
-        self.validate_shape(other, "sub");
-
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] - other.data[i][j];
+                if j >= other.cols {
+                    break;
+                }
+                res.data[i][j] -= other.data[i][j];
             }
         }
         res
@@ -117,10 +129,14 @@ impl Tensor {
     /// assert_eq!(a.data, vec![vec![-4.0, -4.0], vec![-4.0, -4.0]]);
     /// ```
     pub fn sub_mut(&mut self, other: &Tensor) {
-        self.validate_shape(other, "sub_mut");
-
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
+                if j >= other.cols {
+                    break;
+                }
                 self.data[i][j] -= other.data[i][j];
             }
         }
@@ -137,10 +153,10 @@ impl Tensor {
     /// assert_eq!(b.data, vec![vec![-4.0, -3.0], vec![-2.0, -1.0]]);
     /// ```
     pub fn sub_scalar(&self, scalar: f64) -> Tensor {
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] - scalar;
+                res.data[i][j] -= scalar;
             }
         }
         res
@@ -176,12 +192,16 @@ impl Tensor {
     /// assert_eq!(c.data, vec![vec![5.0, 12.0], vec![21.0, 32.0]]);
     /// ```
     pub fn mul(&self, other: &Tensor) -> Tensor {
-        self.validate_shape(other, "mul");
-
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] * other.data[i][j];
+                if j >= other.cols {
+                    break;
+                }
+                res.data[i][j] *= other.data[i][j];
             }
         }
         res
@@ -199,10 +219,14 @@ impl Tensor {
     /// assert_eq!(a.data, vec![vec![5.0, 12.0], vec![21.0, 32.0]]);
     /// ```
     pub fn mul_mut(&mut self, other: &Tensor) {
-        self.validate_mul_shape(other, "mul_mut");
-
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
+                if j >= other.cols {
+                    break;
+                }
                 self.data[i][j] *= other.data[i][j];
             }
         }
@@ -219,10 +243,10 @@ impl Tensor {
     /// assert_eq!(b.data, vec![vec![5.0, 10.0], vec![15.0, 20.0]]);
     /// ```
     pub fn mul_scalar(&self, scalar: f64) -> Tensor {
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] * scalar;
+                res.data[i][j] *= scalar;
             }
         }
         res
@@ -258,12 +282,16 @@ impl Tensor {
     /// assert_eq!(c.data, vec![vec![0.2, 0.25], vec![0.1, 0.5]]);
     /// ```
     pub fn div(&self, other: &Tensor) -> Tensor {
-        self.validate_shape(other, "div");
-
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] / other.data[i][j];
+                if j >= other.cols {
+                    break;
+                }
+                res.data[i][j] /= other.data[i][j];
             }
         }
         res
@@ -281,10 +309,14 @@ impl Tensor {
     /// assert_eq!(a.data, vec![vec![0.2, 0.25], vec![0.1, 0.5]]);
     /// ```
     pub fn div_mut(&mut self, other: &Tensor) {
-        self.validate_shape(other, "div_mut");
-
         for i in 0..self.rows {
+            if i >= other.rows {
+                break;
+            }
             for j in 0..self.cols {
+                if j >= other.cols {
+                    break;
+                }
                 self.data[i][j] /= other.data[i][j];
             }
         }
@@ -301,10 +333,10 @@ impl Tensor {
     /// assert_eq!(b.data, vec![vec![0.2, 0.4], vec![0.6, 0.8]]);
     /// ```
     pub fn div_scalar(&self, scalar: f64) -> Tensor {
-        let mut res = Tensor::zeros(self.rows, self.cols);
+        let mut res = self.clone();
         for i in 0..self.rows {
             for j in 0..self.cols {
-                res.data[i][j] = self.data[i][j] / scalar;
+                res.data[i][j] /= scalar;
             }
         }
         res
