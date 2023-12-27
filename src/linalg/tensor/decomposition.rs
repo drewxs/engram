c75c14a74a96547079dff8e9e49eaeed97b06997
Andrew X. Shah <drewshah0@gmail.com>
2023-12-26 21:13:39 -0700

    feat(linalg/decomposition): create lu decomposition

--- src/linalg/tensor/decomposition.rs
+++ src/linalg/tensor/decomposition.rs
@@ -43,4 +43,39 @@ impl Tensor {
 
         Some(result)
     }
+
+    /// LU decomposition of a square matrix.
+    /// Returns the product of the lower triangular matrix and an upper triangular matrix.
+    ///
+    /// # Examples
+    /// ```
+    /// # use engram::*;
+    /// let t = tensor![[2.0, -1.0, 0.0], [-1.0, 2.0, -1.0], [0.0, -1.0, 2.0]];
+    /// let (l, u) = t.lu().unwrap();
+    /// assert_eq!(l, tensor![[1.0, 0.0, 0.0], [-0.5, 1.0, 0.0], [0.0, -0.6666666666666666, 1.0]]);
+    /// assert_eq!(u, tensor![[2.0, -1.0, 0.0], [0.0, 1.5, -1.0], [0.0, 0.0, 1.3333333333333335]]);
+    /// ```
+    pub fn lu(&self) -> Option<(Self, Self)> {
+        if !self.is_square() {
+            return None;
+        }
+
+        let n = self.data.len();
+
+        let mut l = Tensor::identity(n);
+        let mut u = self.clone();
+
+        for i in 0..self.rows - 1 {
+            for j in i + 1..self.rows {
+                let factor = u.data[j][i] / u.data[i][i];
+                l.data[j][i] = factor;
+
+                for k in i..self.cols {
+                    u.data[j][k] -= factor * u.data[i][k];
+                }
+            }
+        }
+
+        Some((l, u))
+    }
 }
