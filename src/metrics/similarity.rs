f152ad2ad86f3e1ba3285c06e44675e8a508f38c
Andrew X. Shah <drewshah0@gmail.com>
2024-01-26 02:52:32 -0700

    refactor(metrics/similarity): with vector/set utils

--- src/metrics/similarity.rs
+++ src/metrics/similarity.rs
@@ -1,6 +1,12 @@
+use crate::{
+    linalg::dot,
+    utils::{intersection, union},
+};
+
 use super::norm_l2;
 
 /// Compute the cosine similarity between two vectors.
+/// Formula: dot(a, b) / (|a| * |b|)
 ///
 /// # Examples
 ///
@@ -13,13 +19,11 @@ use super::norm_l2;
 /// assert_eq!(cosine_similarity(&a, &c), 0.9914601339836675);
 /// ```
 pub fn cosine_similarity(a: &[f64], b: &[f64]) -> f64 {
-    let dot = a.iter().zip(b.iter()).fold(0.0, |acc, (x, y)| acc + x * y);
-    let norm_a = norm_l2(a);
-    let norm_b = norm_l2(b);
-    dot / (norm_a * norm_b)
+    dot(a, b) / (norm_l2(a) * norm_l2(b))
 }
 
 /// Compute the Jaccard index between two sets.
+/// Formula: |A ∩ B| / |A ∪ B|
 ///
 /// # Examples
 ///
@@ -32,8 +36,8 @@ pub fn cosine_similarity(a: &[f64], b: &[f64]) -> f64 {
 /// assert_eq!(jaccard_index(&a, &c), 0.0);
 /// ```
 pub fn jaccard_index(a: &[bool], b: &[bool]) -> f64 {
-    let intersection = a.iter().zip(b.iter()).filter(|(&a, &b)| a && b).count();
-    let union = a.iter().zip(b.iter()).filter(|(&a, &b)| a || b).count();
+    let intersection = intersection(a, b);
+    let union = union(a, b);
     if union == 0 {
         0.0
     } else {
