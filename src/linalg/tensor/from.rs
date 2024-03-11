0c43eb487dd191c61703cd72f9e32994bd3e4b99
Andrew X. Shah <drewshah0@gmail.com>
2024-03-10 23:47:00 -0600

    feat(tensor/from): impl 1d matrices

--- src/linalg/tensor/from.rs
+++ src/linalg/tensor/from.rs
@@ -1,4 +1,4 @@
-use crate::{Tensor, Tensor2D};
+use crate::{Tensor, Tensor1D, Tensor2D};
 
 impl From<Tensor2D> for Tensor {
     fn from(data: Tensor2D) -> Self {
@@ -17,6 +17,28 @@ impl From<&Tensor2D> for Tensor {
             rows: data.len(),
             cols: data[0].len(),
             data: data.to_owned(),
+            grad: None,
+        }
+    }
+}
+
+impl From<Tensor1D> for Tensor {
+    fn from(data: Tensor1D) -> Self {
+        Tensor {
+            rows: data.len(),
+            cols: 1,
+            data: vec![data],
+            grad: None,
+        }
+    }
+}
+
+impl From<&Tensor1D> for Tensor {
+    fn from(data: &Tensor1D) -> Self {
+        Tensor {
+            rows: data.len(),
+            cols: 1,
+            data: vec![data.to_owned()],
             grad: None,
         }
     }
