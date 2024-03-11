f6bb472bb3db36ceabf6b06e7fe244e6d14c647c
Andrew X. Shah <drewshah0@gmail.com>
2024-03-11 01:44:44 -0600

    refactor(loss): abbreviate enums

--- src/loss/mod.rs
+++ src/loss/mod.rs
@@ -11,22 +11,24 @@ use crate::Tensor;
 /// Loss functions that can be used to train a neural network.
 #[derive(Debug, Clone, Copy, PartialEq, Hash)]
 pub enum Loss {
-    BinaryCrossEntropy,
-    MeanSquaredError,
+    /// Binary cross-entropy.
+    BCE,
+    /// Mean squared error.
+    MSE,
 }
 
 impl Loss {
     pub fn loss(&self, predictions: &Tensor, targets: &Tensor) -> Tensor {
         match self {
-            Loss::BinaryCrossEntropy => bce(predictions, targets),
-            Loss::MeanSquaredError => mse(predictions, targets),
+            Loss::BCE => bce(predictions, targets),
+            Loss::MSE => mse(predictions, targets),
         }
     }
 
     pub fn gradient(&self, predictions: &Tensor, targets: &Tensor) -> Tensor {
         match self {
-            Loss::BinaryCrossEntropy => d_bce(predictions, targets),
-            Loss::MeanSquaredError => d_mse(predictions, targets),
+            Loss::BCE => d_bce(predictions, targets),
+            Loss::MSE => d_mse(predictions, targets),
         }
     }
 }
