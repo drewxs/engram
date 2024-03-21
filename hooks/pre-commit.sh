7eb336dd83be3c6965414979f214d271a5504b26
Andrew X. Shah <drewshah0@gmail.com>
2024-03-21 07:01:14 -0600

    ci(hooks): add clean step to pre-commit

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -3,14 +3,17 @@
 echo "Stashing unstaged changes..."
 git stash save --keep-index --include-untracked
 
+echo "Cleaning artifacts..."
+cargo clean
+
 echo "Formatting code..."
 cargo fmt
 
 echo "Checking code..."
 if ! cargo check; then
-    echo "Code check failed with errors. Popping stash..."
-    git stash pop --quiet
-    exit 1
+	echo "Code check failed with errors. Popping stash..."
+	git stash pop --quiet
+	exit 1
 fi
 
 echo "Code check successful. Popping stash..."
