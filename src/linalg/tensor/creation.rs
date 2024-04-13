82991e9eb96a9f10b885a16816ffaee2c6b13bd1
Andrew X. Shah <drewshah0@gmail.com>
2024-04-12 23:30:21 -0600

    feat(tensor): create `new`, `new_like`

--- src/linalg/tensor/creation.rs
+++ src/linalg/tensor/creation.rs
@@ -1,23 +1,53 @@
 use crate::{Initializer, Tensor};
 
 impl Tensor {
-    /// Creates a new `Tensor` of 0s with the specified number of rows and columns.
+    /// Creates a new `Tensor` with the specified number of rows, columns, and value.
     ///
     /// # Examples
     /// ```
     /// # use engram::*;
-    /// let t1 = Tensor::zeros(2, 3);
-    /// let t2 = Tensor::zeros(3, 2);
+    /// let t1 = Tensor::new(2, 3, 0.0);
+    /// let t2 = Tensor::new(3, 2, 0.0);
     /// assert_eq!(t1.data, vec![vec![0.0, 0.0, 0.0], vec![0.0, 0.0, 0.0]]);
     /// assert_eq!(t2.data, vec![vec![0.0, 0.0], vec![0.0, 0.0], vec![0.0, 0.0]]);
     /// ```
-    pub fn zeros(rows: usize, cols: usize) -> Tensor {
+    pub fn new(rows: usize, cols: usize, value: f64) -> Tensor {
         Tensor {
             rows,
             cols,
-            data: vec![vec![0.0; cols]; rows],
+            data: vec![vec![value; cols]; rows],
             grad: None,
         }
+    }
+
+    /// Creates a new `Tensor` with the same shape as the provided `Tensor`, filled with the specified value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use engram::*;
+    /// let t1 = Tensor::new(2, 3, 0.0);
+    /// let t2 = Tensor::new_like(&t1, 0.0);
+    /// assert_eq!(t2.data, vec![vec![0.0, 0.0, 0.0], vec![0.0, 0.0, 0.0]]);
+    /// ```
+    pub fn new_like(other: &Tensor, value: f64) -> Tensor {
+        Tensor::new(other.rows, other.cols, value)
+    }
+
+    pub fn zeros(rows: usize, cols: usize) -> Tensor {
+        Tensor::new(rows, cols, 0.0)
+    }
+
+    pub fn zeros_like(other: &Tensor) -> Tensor {
+        Tensor::zeros(other.rows, other.cols)
+    }
+
+    pub fn ones(rows: usize, cols: usize) -> Tensor {
+        Tensor::new(rows, cols, 1.0)
+    }
+
+    pub fn ones_like(other: &Tensor) -> Tensor {
+        Tensor::ones(other.rows, other.cols)
     }
 
     /// Creates a new `Tensor` with the specified size, initialized to the identity matrix.
@@ -34,53 +64,6 @@ impl Tensor {
             res.data[i][i] = 1.0;
         }
         res
-    }
-
-    /// Creates a new `Tensor` of 0s with the same shape as the provided `Tensor`.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use engram::*;
-    /// let t1 = Tensor::zeros(2, 3);
-    /// let t2 = Tensor::zeros_like(&t1);
-    /// assert_eq!(t2.data, vec![vec![0.0, 0.0, 0.0], vec![0.0, 0.0, 0.0]]);
-    /// ```
-    pub fn zeros_like(other: &Tensor) -> Tensor {
-        Tensor::zeros(other.rows, other.cols)
-    }
-
-    /// Creates a new `Tensor` of 1s with the specified number of rows and columns.
-    ///
-    /// # Examples
-    /// ```
-    /// # use engram::*;
-    /// let t1 = Tensor::ones(2, 3);
-    /// let t2 = Tensor::ones(3, 2);
-    /// assert_eq!(t1.data, vec![vec![1.0, 1.0, 1.0], vec![1.0, 1.0, 1.0]]);
-    /// assert_eq!(t2.data, vec![vec![1.0, 1.0], vec![1.0, 1.0], vec![1.0, 1.0]]);
-    /// ```
-    pub fn ones(rows: usize, cols: usize) -> Tensor {
-        Tensor {
-            rows,
-            cols,
-            data: vec![vec![1.0; cols]; rows],
-            grad: None,
-        }
-    }
-
-    /// Creates a new `Tensor` of 1s with the same shape as the provided `Tensor`.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use engram::*;
-    /// let t1 = Tensor::ones(2, 3);
-    /// let t2 = Tensor::ones_like(&t1);
-    /// assert_eq!(t2.data, vec![vec![1.0, 1.0, 1.0], vec![1.0, 1.0, 1.0]]);
-    /// ```
-    pub fn ones_like(other: &Tensor) -> Tensor {
-        Tensor::ones(other.rows, other.cols)
     }
 
     /// Creates a new `Tensor` with the specified number of rows and columns, initialized using the provided initializer.
