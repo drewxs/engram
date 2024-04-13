026dc9602ce32acd3be7572f54d70c1f49a07b1c
Andrew X. Shah <drewshah0@gmail.com>
2024-04-12 23:30:36 -0600

    refactor(nn): rm network

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -1,13 +1,11 @@
 mod dataset;
 mod initializer;
 mod layer;
-mod network;
 mod regularization;
 mod sequential;
 
 pub use dataset::*;
 pub use initializer::*;
 pub use layer::*;
-pub use network::*;
 pub use regularization::*;
 pub use sequential::*;
